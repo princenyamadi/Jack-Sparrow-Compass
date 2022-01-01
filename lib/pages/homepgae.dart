673f8f0b8e6cd86e20f87cc7f9335ff86754f68e
princenyamadi <princenyamadi.pn@gmail.com>
2022-01-01 13:11:28 +0000

    renders paints

---
+++ lib/pages/homepgae.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:jack_sparrow_compass/pages/clock_view.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: const Color(0xFF2D2F41),
+        child: ClockView(),
+      ),
+    );
+  }
+}
